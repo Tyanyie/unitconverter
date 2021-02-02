84702aa239261bf16dcf3d9b35fa316d92f73bd2
Admin <zhout17@mytru.ca>
2021-02-01 19:21:34 -0800

    finished

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.unitconverter;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,36 +24,36 @@ public class MainActivity extends AppCompatActivity
 
         EditText inputLeft = (EditText)findViewById(R.id.inputLeft);
         EditText inputRight = (EditText)findViewById(R.id.inputRight);
+        EditText inputLeft2 = (EditText)findViewById(R.id.inputLeft2);
+        EditText inputRight2 = (EditText)findViewById(R.id.inputRight2);
 
-        TextView textLeft = (TextView)findViewById(R.id.textLeft);
-        TextView textRight = (TextView)findViewById(R.id.textRight);
-
-        /*
-        * Todo:
-        *  - input field can not enter negative value (it only works on input number first and then negative sign)         <----------------
-        *  - app will auto close after delete value in input field                                                         <----------------
-        * */
-
-            inputLeft.addTextChangedListener(new TextWatcher()
+        Toast.makeText(getApplicationContext(),"Welcome to Unit Converter by Tianyi Zhou",Toast.LENGTH_SHORT).show();
+        inputLeft.addTextChangedListener(new TextWatcher()
             {
                 Editable input;
-                float result;
+                double result;
+                double a;
 
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after)
                 {
                     input = inputLeft.getEditableText();
                     Log.v(TAG, "Before Text Changed");
-
                 }
 
                 //Example: (50°F - 32) x .5556 = 10°C
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count)
                 {
-                    //input = inputLeft.getEditableText();
-                    float a = Float.parseFloat(String.valueOf(input));
-                    result = (float) ((a - 32.0) * 0.5556);
+                    try
+                    {
+                        a = Float.parseFloat(String.valueOf(input));
+                        result = (a - 32) * 0.5556;
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        Log.v(TAG, "NumberFormatException");
+                    }
                     Log.v(TAG, "On Text Changed");
                 }
 
@@ -66,25 +65,46 @@ public class MainActivity extends AppCompatActivity
                 }
             });
 
-
-
-
-
-
-
-
             try
             {
                 this.getSupportActionBar().hide();
             }
             catch (NullPointerException e)
             {
-                System.out.print("Null Pointer Exception Occurred");
+                Log.v(TAG, "Null pointer exception");
             }
-
         Log.v(TAG, "End of On create");
 
+            inputLeft2.addTextChangedListener(new TextWatcher()
+            {
+                Editable input2;
+                double b;
+                double result2;
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after)
+                {
+                    input2 = inputLeft2.getEditableText();
+                }
 
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count)
+                {
+                    try
+                    {
+                        b = Float.parseFloat(String.valueOf(input2));
+                        result2 = (b / 0.5556) + 32;
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        Log.v(TAG, "NumberFormatException2");
+                    }
+                }
 
+                @Override
+                public void afterTextChanged(Editable s)
+                {
+                    inputRight2.setText(String.valueOf(result2));
+                }
+            });
     }
 }
